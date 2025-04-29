68085cb61d3b59e164e2ad8785f79687055919ac
Kevin McKee <kevinmmckee@gmail.com>
2025-04-29 12:26:17 -0700

    WIP

--- construction/utilities.py
+++ construction/utilities.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import numpy as np
 
 def csv_to_list(filename):
     """
@@ -31,7 +32,8 @@ def jsonl_to_list(file_path):
     return data
     
 def write_jsonl(data, filename):
-    """Writes a list of dictionaries to a JSONL file.
+    """
+    Writes a list of dictionaries to a JSONL file.
 
     Args:
         data: A list of dictionaries to be written to the file.
@@ -42,3 +44,17 @@ def write_jsonl(data, filename):
             json.dump(item, f)
             f.write('\n')
 
+def split_train_val_test(data, ratio=[0.8, 0.1, 0.1]):
+    """
+    Splits the list into training, validation, and testing lists.
+
+    Args:
+        data: A list of data
+        ratio: The ratios to split by
+    """
+    train_r, val_r, test_r = ratio
+    assert(np.sum(ratio) == 1.0)  # makes sure the splits make sense
+    # note we only need to give the first 2 indices to split, the last one it returns the rest of the list or empty
+    indicies_for_splitting = [int(len(data) * train_r), int(len(data) * (train_r+val_r))]
+    train, val, test = np.split(data, indicies_for_splitting)
+    return list(train), list(val), list(test)
